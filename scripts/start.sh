3ec1a65f138342b6e96ba89f78e2c210e687d8b7
Brody <brodyover@gmail.com>
2024-05-02 08:42:44 -0400

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,7 +9,7 @@ while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 0.25; d
 echo Database is now available
 
 parallel --ungroup --halt now,done=1 ::: \
-    "exec ./start_odoo.sh" \
-    "exec ./start_caddy.sh"
+    "./start_odoo.sh" \
+    "./start_caddy.sh"
 
 false
